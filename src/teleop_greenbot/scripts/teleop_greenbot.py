d6d56f44eb14ee251f8953212295cf5df465aea8
Alexis Pascual <ap.alexispascual@gmail.com>
2020-11-04 13:51:44 -0500

    Modifying cmd_vel subscriber into a class

--- src/teleop_greenbot/scripts/teleop_greenbot.py
+++ src/teleop_greenbot/scripts/teleop_greenbot.py
@@ -1,21 +1,57 @@
 #!/usr/bin/env python
 import roslib; roslib.load_manifest('teleop_greenbot')
 import rospy
+import serial
+import time
 from geometry_msgs.msg import Twist
 
-def callback(msg):
+class TeleopGreenbot:
+    def __init__(self):
+        rospy.init_node('teleop_greenbot', anonymous=True)
 
-    rospy.loginfo(f"Linear: x = {msg.linear.x}, y = {msg.linear.y} z = {msg.linear.z}")
-    rospy.loginfo(f"Angular: x = {msg.angular.x}, y = {msg.angular.y} z = {msg.angular.z}")
+        rospy.Subscriber("/cmd_vel", Twist, self.handle_twist_message)
+
+        self.udoo_serial = self.initialize_arduino()
+
+    # ------------------------------------------------------------------------------
+    @staticmethod
+    def initialize_arduino():
+        try:
+            ser = serial.Serial('/dev/ttyACM0',115200,timeout=1)
+            ser.flushOutput()
+
+            rospy.loginfo("Connected to Arduino!")
+
+        except Exception as e:
+
+            rospy.logerr(f"Arduino Initialization error: {e}")
+
+        finally:
+
+            return ser
+
+    def handle_twist_message(self, msg):
+
+        rospy.loginfo(f"Linear: x = {msg.linear.x}, y = {msg.linear.y} z = {msg.linear.z}")
+        rospy.loginfo(f"Angular: x = {msg.angular.x}, y = {msg.angular.y} z = {msg.angular.z}")
+
+    def sendToArduino(self, msg):
+
+        self.udoo_serial(b"{msg}")
+
+    def start(self):
+        # spin() simply keeps python from exiting until this node is stopped
+        rospy.spin()
     
-def listener():
+# def listener():
 
-    rospy.init_node('teleop_greenbot', anonymous=True)
+#     rospy.init_node('teleop_greenbot', anonymous=True)
 
-    rospy.Subscriber("/cmd_vel", Twist, callback)
+#     rospy.Subscriber("/cmd_vel", Twist, callback)
 
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+#     # spin() simply keeps python from exiting until this node is stopped
+#     rospy.spin()
 
 if __name__ == '__main__':
-    listener()
+    teleop_greenbot = TeleopGreenbot()
+    teleop_greenbot.start()

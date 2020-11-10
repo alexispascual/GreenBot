fc28f0c77ed1c1e5d424bfd64da77fe4dbc3b6de
Alexis Pascual <ap.alexispascual@gmail.com>
2020-11-10 13:20:20 -0500

    Creating video streaming package

---
+++ src/video_stream/scripts/video_stream_publisher.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import cv2
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+class ImageStreamer:
+	def __init__(self):
+		
+
+if __name__ == '__main__':
+	main()

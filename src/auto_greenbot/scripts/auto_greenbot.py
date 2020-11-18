0e13bb7cd5f05a73c83db604aef260482a891abb
Alexis Pascual <ap.alexispascual@gmail.com>
2020-11-18 14:52:48 -0500

    Adding autonomous greenbot package

---
+++ src/auto_greenbot/scripts/auto_greenbot.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import rospy
+import serial
+from std_msgs.msg import String
+
+class AutonomousGreenbot:
+	
+	def __init__(self, arg):
+		pass
+
+	def start():
+		pass
+
+if __name__ == '__main__':
+	auto_greenbot = AutonomousGreenbot()
+	auto_greenbot.start

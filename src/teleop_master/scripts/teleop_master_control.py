9ec069908fe69c5362decc773773549698dff0d6
Alexis Pascual <ap.alexispascual@gmail.com>
2020-11-19 12:50:09 -0500

    Adding autonomous/manual switch.

--- src/teleop_master/scripts/teleop_master_control.py
+++ src/teleop_master/scripts/teleop_master_control.py
@@ -3,6 +3,7 @@ import roslib; roslib.load_manifest('teleop_master')
 import rospy
 import serial
 import time
+import constants as c
 from teleop_master.msg import MotionCommand
 from teleop_master.msg import MasterCommand
 from sensor_msgs.msg import Joy
@@ -23,23 +24,29 @@ class TeleopMaster:
         self.master_cmd_publisher = rospy.Publisher('master_cmd', MasterCommand, queue_size=10)
 
         # Define buttons
-        self.start_control = False
-        self.start_control_button = 9
-        self.enable_button = 0
-        self.x_axis_index = 5
-        self.z_axis_index = 4
-        self.mast_button_up = 4
-        self.mast_button_down = 5
-        self.speed_button_up = 6
-        self.speed_button_down = 7
+        self.teleop_control_button = c.START_BUTTON
+        self.autonomous_control_button = c.BACK_BUTTON
+        self.enable_button = c.X_BUTTON
+        self.x_axis_index = c.UP_DOWN_AXIS
+        self.z_axis_index = c.LEFT_RIGHT_AXIS
+        self.mast_button_up = c.LB_BUTTON
+        self.mast_button_down = c.RB_BUTTON
+        self.speed_button_up = c.LT_BUTTON
+        self.speed_button_down = c.RT_BUTTON
         self.enable_control = False
         self.new_command = False
+
+        # Initialize rover status
+        self.control_status = c.STANDBY
 
         # Initialize control modes and default speed
         self.x = 0
         self.z = 0
         self.speed = 127
         self.mast_control = 0
+
+        # Initialize rate @ 10Hz
+        self.rate = rospy.Rate(10)
 
     def getMotionCommand(self):
 
@@ -110,42 +117,56 @@ class TeleopMaster:
         finally:
             self.new_command = True
         
-
         return
 
-    def start(self):
+    def TeleopMode(self):
+        while (self.control_status == 1):
+            if self.new_command:
+                # Get command string
+                command_message = self.getMotionCommand()
 
-        # Initialize rate @ 10Hz
-        rate = rospy.Rate(10)
+                print(command_message)
+
+                # Publish command message
+                self.command_publisher.publish(command_message)
+
+                self.new_command = False
+                
+            self.rate.sleep()
+
+    def AutonomousMode(self):
+        while (self.control_status == 2):
+            # TODO: Figure out what master does when rover is autonomous
+            pass
+
+
+    def start(self):
 
         # Initialize Publisher
         while not rospy.is_shutdown():
 
             # Wait for subscribers
-            if self.command_publisher.get_num_connections() == 0:
-                rospy.loginfo("Waiting for subscribers...")
+            if self.master_cmd_publisher.get_num_connections() == 0:
+                rospy.loginfo("Waiting for Geenbot to subscribe...")
                 rospy.sleep(1)
 
             # Once Greenbot is subscribed, start publishing
-            else:
-                if not self.start_control:
-                    rospy.loginfo("Control is currently disabled. Press Start to begin teleop mode.")
-                    rospy.sleep(1)
+            elif (self.master_cmd_publisher.get_num_connections() & self.control_status == c.STANDBY):
+                rospy.loginfo("Greenbot is in Standby mode. Press 'Start' to begin teleop mode.")
+                rospy.loginfo("To begin Autonomous mode, line up the rover to the first QR code and press 'Back'")
+                rospy.sleep(1)
 
-                # Check if there's a new command
-                else:
-                    if self.new_command:
-                        # Get command string
-                        command_message = self.getMotionCommand()
+            # Greenbot in Teleop mode
+            elif (self.master_cmd_publisher.get_num_connections() & self.control_status == c.TELEOP_MANUAL):
+                self.master_cmd_publisher.publish(c.TELEOP_MANUAL)
+                self.TeleopMode()
 
-                        print(command_message)
+            # Greenbot in Autonomous mode        
+            elif(self.master_cmd_publisher.get_num_connections() & self.control_status == c.AUTONOMOUS):
+                self.master_cmd_publisher.publish(c.AUTONOMOUS)
+                self.AutonomousMode()
 
-                        # Publish command message
-                        self.command_publisher.publish(command_message)
 
-                        self.new_command = False
-
-                rate.sleep()
 
 if __name__ == '__main__':
     teleop_master = TeleopMaster()

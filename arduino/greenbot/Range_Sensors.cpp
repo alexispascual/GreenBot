080ace986501e5e87ee9f5bd637265031a28e17d
alexispascual <ap.alexispascual@gmail.com>
2020-11-25 10:18:11 -0500

    Adding skeleton code for Range_Sensor library

--- arduino/greenbot/Range_Sensors.cpp
+++ arduino/greenbot/Range_Sensors.cpp
@@ -5,8 +5,23 @@
 //                   Constructor											//
 //--------------------------------------------------------------------------//
 
-bool Range_Sensors::Range_Sensors() {
+Range_Sensors::Range_Sensors(uint8_t* trigger_pins, uint8_t* echo_pins, uint8_t num_sensors) {
 
 
+}
+
+void Range_Sensors::Activate_Sensors() {
+
+}
+
+float Range_Sensors::Get_Front_Distance() {
+
+}
+
+float Range_Sensors::Get_Rear_Distance() {
+
+}
+
+float* Range_Sensors::Get_Distances() {
 
 }
--- arduino/greenbot/Range_Sensors.h
+++ arduino/greenbot/Range_Sensors.h
@@ -8,12 +8,26 @@
 #include "Arduino.h"
 
 class Range_Sensors {
-
 //--------------------------------------------------------------------------//
 //                                  Definitions                             //
 //--------------------------------------------------------------------------//
+	private:
 
+		#define NUM_SENSORS 4
 
+		uint8_t trigger_pins[NUM_SENSORS] = {0, 2};
+		uint8_t echo_pins[NUM_SENSORS] = {1, 3};
+
+		float front_distance;
+		float rear_distance;
+
+		void Activate_Sensors();
+		
+	public:
+		Range_Sensors(uint8_t* trigger_pins, uint8_t* echo_pins);
+		float Get_Front_Distance();
+		float Get_Rear_Distance();
+		float* Get_Distances();
 }
 
 #endif

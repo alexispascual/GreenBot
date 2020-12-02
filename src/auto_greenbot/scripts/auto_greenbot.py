b321140d01581f0ae2a243fc9548281cf2f59db7
Alexis Pascual <ap.alexispascual@gmail.com>
2020-12-02 16:07:33 -0500

    Modifying speeds and durations

--- src/auto_greenbot/scripts/auto_greenbot.py
+++ src/auto_greenbot/scripts/auto_greenbot.py
@@ -25,13 +25,13 @@ class AutonomousGreenbot:
         # Initialize default durations
         self.drive_forward_duration = 5
         self.imaging_duration = 5
-        self.turn_maneuver_forward_duration = 10
+        self.turn_maneuver_forward_duration = 7
         self.turn_maneuver_turn_duration = 3.5
         self.turn_around_duration = 9.0
 
         # Define forward and slow speeds    
-        self.gb_default_speed = 22
-        self.gb_slow_speed = 22
+        self.gb_default_speed = 24
+        self.gb_slow_speed = 24
         self.gb_turning_speed = 102
 
         # Initialize qr_subscriber object

6ed5d0cd99334a05a0b8785902b7811ab4aef3de
Alexis Pascual <ap.alexispascual@gmail.com>
2020-12-09 17:48:37 -0500

    Parsing serial messages correctly now

--- arduino/parse_serial/parse_serial.ino
+++ arduino/parse_serial/parse_serial.ino
@@ -10,7 +10,11 @@ char raw_chars[num_chars];
 void setup() {
   // put your setup code here, to run once:
   
-  Serial.begin(9600);
+  Serial1.begin(115200);
+  delay(100);
+  Serial1.println("Start");
+
+  Serial.begin(115200);
   delay(100);
   Serial.println("Start");
 
@@ -25,19 +29,15 @@ void ReceiveSerialData() {
     char temp_char;
     float temp_float;
  
-    while (Serial.available() > 0 && new_data == false) {
-        Serial.println("Receiving data...");
-        temp_char = Serial.read();
+    while (Serial1.available() > 0 && new_data == false) {
+        temp_char = Serial1.read();
 
         if (in_progress == true) {
-            if (temp_char != end_marker) {
+          
               raw_chars[i] = temp_char;
-                i++;
-                if (i >= num_chars) {
-                    i = num_chars - 1;
-                }
-            }
-            else {
+              i++;
+           
+            if (i == 2) {
                 raw_chars[i] = '\0'; // terminate the string
                 in_progress = false;
                 i = 0;
@@ -62,13 +62,12 @@ void ParseRawChars() {
 void PrintData() {
   
   if (new_data) {
-    Serial.println("Printing data");
     new_data = false;
     
     Serial.print("Left wheel: ");
     Serial.print(left_wheel_speed);
-    Serial.print("Right wheel: ");
-    Serial.print(right_wheel_speed);
+    Serial.print(" Right wheel: ");
+    Serial.println(right_wheel_speed);
     
   }
 }
